725663666bd298e79253c3f23e6c98d7653bb41b
medioqrity <medioqrity@gmail.com>
2022-04-21 15:40:39 -0400

    add BandComponent (not implemented yet)

---
+++ Source/BandComponent.h
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+    BandWrapper.h
+    Created: 21 Apr 2022 3:31:37pm
+    Author:  medioqrity
+
+  ==============================================================================
+*/
+
+#ifndef __hdr_BandComponent_h__
+#define __hdr_BandComponent_h__
+
+#include <JuceHeader.h>
+#include "PluginWrapperComponent.h"
+
+class BandComponent : juce::Component {
+private:
+
+};
+
+#endif // __hdr_BandComponent_h__
